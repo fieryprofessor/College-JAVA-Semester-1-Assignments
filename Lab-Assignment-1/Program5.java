379fdd8ef54ea1950d5eec5e87c963b95a0c8d86
Om Singh <ninjaplays792020@gmail.com>
2023-11-05 10:06:13 +0530

    Added programs till Q6

---
+++ Lab-Assignment-1/Program5.java
@@ -0,0 +1,7 @@
+public class Program5 {
+    public static void main(String args[]){
+        String name= "Rishi";
+        String greeting= "Good Morning";
+        System.out.print("Hey "+name+", "+greeting+"!");
+    }
+}
---
+++ Lab-Assignment-1/Program6.java
@@ -0,0 +1,9 @@
+public class Program6 {
+    public static void main(String args[]){
+        String name= "Amit Kumar";
+        int account_number= 123456;
+        double balance= 7654.98;
+        System.out.println("\"My name is "+name+" bearing account number "+account_number);
+        System.out.print("having balance "+balance+"\"");
+    }
+}

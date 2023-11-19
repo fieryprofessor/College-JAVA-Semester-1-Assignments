fb5e2cab631b964eb78bfff213d7dfec949adb0d
Om Singh <ninjaplays792020@gmail.com>
2023-11-19 13:57:30 +0530

    corrected program1

--- Lab-Assignment-2/Keyboard-input/Program1.java
+++ Lab-Assignment-2/Keyboard-input/Program1.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class Program1{
 public static void main(String args[]){
 Scanner sc = new Scanner(System.in);
-System.out.print("Enter a degree in Farenheit:");
+System.out.print("Enter a degree in Fahrenheit:");
 double f=sc.nextDouble();
 double c=(5.0/9)*(f-32);
-System.out.println(f+" Farenheit is "+c+" Celcius");
+System.out.println(f+" Fahrenheit is "+c+" Celcius");
 sc.close();
 }
 }
